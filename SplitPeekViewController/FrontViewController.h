80c166d90160daa809fb08563484a3a900e2891f
PlenipotentSS <stevensonhoyt@icloud.com>
2014-01-31 13:46:56 -0800

    Added gitiginore

---
+++ SplitPeekViewController/FrontViewController.h
@@ -0,0 +1,13 @@
+//
+//  FrontViewController.h
+//  SplitPeekViewController
+//
+//  Created by Stevenson on 1/31/14.
+//  Copyright (c) 2014 Steven Stevenson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FrontViewController : UIViewController
+
+@end
